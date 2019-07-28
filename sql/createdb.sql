138fc68802766a152e9a77fe981ee2080089bb8f
Mika Katua <mikakatua@gmail.com>
2019-07-28 09:38:14 +0200

    new structure

--- sql/createdb.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `guestbook` (
-`id` int(4) NOT NULL auto_increment,
-`name` varchar(65) NOT NULL,
-`email` varchar(65) NOT NULL,
-`comment` varchar(255) NOT NULL,
-`datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
